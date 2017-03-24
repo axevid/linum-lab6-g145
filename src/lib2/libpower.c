77ba15a8518f3873293b5baa31815c3fc93568e2
Axel Vidmark <Axel Vidmark>
2017-03-24 13:29:06 +0100

    created all files and test

--- src/lib2/libpower.c
+++ src/lib2/libpower.c
@@ -1,1 +1,17 @@
-# file for power calc
+// # file for power calc
+
+#include <libpower.h>
+#include <math.h>
+
+float calc_power_r(float volt, float resistance) {
+	float p
+	p = powf(volt, 2.)/resistance
+	return p
+
+}
+
+float calc_power_i(float volt, float current){
+	float p;
+	p = volt * current;
+	return p
+}
