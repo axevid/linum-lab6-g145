14787eb42bf2e14a9cc0be61f2272860df7bd17d
Antero Metso <antero.metso@gmail.com>
2017-04-12 04:04:07 +0300

    Simple Doxygen comments added to libresistance.h and test_libresistance.c

--- src/lib1/libresistance.h
+++ src/lib1/libresistance.h
@@ -1,10 +1,29 @@
-/*
-# Header file for libresistance
+/**
+ * @file   libresistance.h
+ * @author Jouni Metso <antero.metso@gmail.com>
+ * @version 1.0
+ * @brief  Function prototype for calc_resistance function.
+ * @see See libresistance.c for better description of the function.
 */
 #ifndef __LIBRESISTANCE_H__
 #define __LIBRESISTANCE_H__
 
-/* prototype for calc_resistance */ 
+/**  @brief Calculates the equivalent resistance for a simple resistor network.
+ *
+ *   Write a library, libresistance.so, with functions to calculate the 
+ *   total resistance for different coupled resistances. The resistances to 
+ *   be connected in parallel or in series. You do not count on mixed couplings.
+ *
+ *   A value of 0 will be returned if resistance is zero when operating 
+ *   in parallel, ie R1 || R2 = 0 if R1 or R2 is 0Ohm.
+ *
+ *   The library will not crash if a null pointer is sent to the function, 
+ *   ie if the array = 0.
+ *   If the arguments are incorrect, the function will return -1.
+ *
+ *   The return value is the resulting resistance. 
+ *
+ */
 float calc_resistance (int count, char conn, float * array);
 
 #endif
--- src/lib1/test_libresistance.c
+++ src/lib1/test_libresistance.c
@@ -1,6 +1,19 @@
+/**
+ *@file test_libresistance.c
+ *@author Jouni Metso
+ *@date 04/12/2017
+ *@brief File for a program for testing libresistance.
+ *
+ * The program runs several test cases against libresistance 
+ * and displays results.
+ *
+ */
 #include <stdio.h>
 #include "libresistance.h"
-
+/**  @brief Main program
+ *   @return Returns zero in the end.
+ *
+ */
 int main(void)
 {
   float test1[2]={1.0f,1.0f};
@@ -9,54 +22,54 @@ int main(void)
   float test4[3]={2.0f,4.0f,6.0f};
   float test5[3]={5.0f};
   
-  // float calc_resistance (int count, char conn, float * array)
-    printf("Parallel resistance with 0 1 ohm resistors with array of 3 1 ohm resistors \n %f\n", calc_resistance(0,'P', test1));
+  printf("Parallel resistance with 0 1 ohm resistors with array of 3 1 ohm resistors \n %f\n", calc_resistance(0,'P', test1));
   printf("it should return 0  because array is not a null pointer\n");
-
-    printf("Parallel resistance with 0 resistors with null pointer as array \n %f\n", calc_resistance(0,'P', NULL));
+  
+  printf("Parallel resistance with 0 resistors with null pointer as array \n %f\n", calc_resistance(0,'P', NULL));
   printf("it should return -1 (ERROR)  because array is a null pointer\n");
-
-
-printf("Parallel resistance with 2 1 ohm resistors\n %f\n", calc_resistance(2,'P', test1));
+  
+  
+  printf("Parallel resistance with 2 1 ohm resistors\n %f\n", calc_resistance(2,'P', test1));
   printf("it should be 0.5\n");
   
-printf("Series resistance with 3 1 ohm resistors\n %f\n", calc_resistance(3,'S', test2));
+  printf("Series resistance with 3 1 ohm resistors\n %f\n", calc_resistance(3,'S', test2));
   printf("it should be 3\n");
-  printf("Series resistance with 2 1 ohm and 1 0 ohm resistors\n %f\n", calc_resistance(3,'S', test3));
+  printf("Series resistance with 2 1 ohm and 1 0 ohm resistors\n %f\n", 
+	 calc_resistance(3,'S', test3));
   printf("it should be 2\n");
   printf("*Parallel resistance with 1 0 ohm resistors\n %f\n", calc_resistance(1,'P', 0));
   printf("it should be -1 (ERROR)\n");
   printf("Series resistance with 1 5 ohm resistor\n %f\n", calc_resistance(1,'S', test5));
   printf("it should be 5\n");
-
-printf("Series resistance with 1 5 ohm resistors and count of 2\n %f\n", 
-calc_resistance(2,'S',test5));
+  
+  printf("Series resistance with 1 5 ohm resistors and count of 2\n %f\n", 
+	 calc_resistance(2,'S',test5));
   printf("it should always return -1 (ERROR) but it does not\n");
-
-printf("Series resistance with 1 5 ohm resistors and count of -1\n %f\n", 
-calc_resistance(2,'S',test5));
+  
+  printf("Series resistance with 1 5 ohm resistors and count of -1\n %f\n", 
+	 calc_resistance(2,'S',test5));
   printf("it should always return -1 (ERROR) but it does not\n");
-
-printf("*Parallel resistance with 2 1 ohm and 1 0 ohm resistors\n %f\n", calc_resistance(3,'P', test3));
+  
+  printf("*Parallel resistance with 2 1 ohm and 1 0 ohm resistors\n %f\n", calc_resistance(3,'P', test3));
   printf("it should be -1.0 (ERROR)\n");
-
-printf("Parallel resistance with 3 1 ohm resistors\n %f\n", calc_resistance(3,'P', test2));
+  
+  printf("Parallel resistance with 3 1 ohm resistors\n %f\n", calc_resistance(3,'P', test2));
   printf("it should be 0.3333\n");
-
-printf("Parallel resistance with 1 5 ohm resistors\n %f\n", 
-calc_resistance(1,'P',test5));
+  
+  printf("Parallel resistance with 1 5 ohm resistors\n %f\n", 
+	 calc_resistance(1,'P',test5));
   printf("it should be 5\n");
-
-printf("Parallel resistance with 1 5 ohm resistors and count of 2\n %f\n", 
-calc_resistance(2,'P',test5));
+  
+  printf("Parallel resistance with 1 5 ohm resistors and count of 2\n %f\n", 
+	 calc_resistance(2,'P',test5));
   printf("it should always return -1 (ERROR), but it does not.\n");
-
-printf("**Parallel resistance with 2 ohm resistor 4 ohm resistor and 6 ohm resistor \n %f\n", 
-calc_resistance(3,'P',test4));
+  
+  printf("**Parallel resistance with 2 ohm resistor 4 ohm resistor and 6 ohm resistor \n %f\n", 
+	 calc_resistance(3,'P',test4));
   printf("it should return 1.0909\n");
-
-
-printf("Parallel resistance with null pointer\n %f\n", calc_resistance(3,'P', NULL));
+  
+  
+  printf("Parallel resistance with null pointer\n %f\n", calc_resistance(3,'P', NULL));
   printf("it should return -1 (ERROR)\n");
   
   return 0;
